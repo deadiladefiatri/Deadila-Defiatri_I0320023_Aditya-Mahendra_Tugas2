03927ea98b9602fc85ccc086c8e574e158ccfd5b
Deadila Defiatri <deadiladefiatri04@gmail>
2021-03-12 00:13:46 +0700

    Pengerjaan Tugas

---
+++ I0320023_Soal2_Tugas 2.py
@@ -0,0 +1,37 @@
+print ("================Identitas Diri================")
+print("")
+Nama_Lengkap                    = "Deadila Defiatri"
+Nama_Panggilan                  = "Dea"
+Tempat_Lahir                    = "Batusangkar, Sumatera Barat"
+Tanggal_Sekarang                = 11
+Bulan_Sekarang                  = 3
+Tahun_Sekarang                  = 2021
+Tanggal_Lahir                   = 6
+Bulan_Lahir                     = 11
+Tahun_Lahir                     = 2001
+Umur_bulan                      = "((Tanggal_Sekarang-Tanggal_Lahir/30) + (Bulan_Sekarang-Bulan_Lahir) + (Tahun_Sekarang-Tanggal_Lahir*12))"
+Alamat                          = "Perumahan. Pemda No.7 Saruaso Barat Kab. Tanah Datar, Sumatera Barat"
+Kode_Pos                        = 27281
+Agama                           = "Islam"
+Jenis_Kelamin                   = "Perempuan"
+No_hp                           = "082389823191"
+Id_Line                         = "deadila6"
+No_Whatsapp                     = "082389823191"
+Instagram                       = "deadiladefiatri_"
+Email                           = "deadiladefiatri04@gmail.com"
+Pekerjaan                       = "Mahasiswi"
+Jurusan                         = "Teknik Industri"
+Fakultas                        = "Teknik"
+Perguruan_Tinggi                = "Universitas Sebelas Maret"
+Jalur_Masuk                     = "SNMPTN"
+NIM                             = "I0320023"
+Asal_SMA                        = "SMAN 3 Batusangkar"
+Hobi                            = "Fotografi dan Mendengar Musik"
+Tinggi_Badan                    = "158 cm"
+Berat_Badan                     = "45"
+Ukuran_Sepatu                   = "39 cm"
+Golongan_Darah                  = "O"
+Cita_Cita                       = "Aktuaris"
+print (" Nama lengkap     :",Nama_Lengkap, "\n", "Nama Panggilan   :",Nama_Panggilan,"\n", "Tempat Lahir     :",Tempat_Lahir, "\n","Umur             :",Umur_bulan, "\n","Perguruan Tinggi :",  Perguruan_Tinggi, "\n","Alamat           :", Alamat, "\n", "Kode Pos         :", Kode_Pos, "\n","Agama            :", Agama, "\n","Jenis Kelamin    :",  Jenis_Kelamin, "\n","No. Hp           :", No_hp,"\n","Id Line          :", Id_Line,"\n","No Whatsapp      :", No_Whatsapp,"\n","Instagram        :", Instagram,"\n","Email            :", Email, "\n","Pekerjaan        : ", Pekerjaan, "\n","Jurusan          :", Jurusan, "\n","Fakultas         :", Fakultas, "\n","Perguruan Tinggi :", Perguruan_Tinggi, "\n","Jalur Masuk      :", Jalur_Masuk, "\n","NIM              :", NIM,"\n","Asal Sekolah     :", Asal_SMA,"\n", "Hobi             :", Hobi, "\n", "Tinggi Badan     :", Tinggi_Badan, "\n", "Berat Badan      :", Berat_Badan, "\n", "Ukuran Sepatu    :", Ukuran_Sepatu, "\n", "Golongan Darah   :", Golongan_Darah, "\n", "Cita - Cita      :", Cita_Cita)
+
+
