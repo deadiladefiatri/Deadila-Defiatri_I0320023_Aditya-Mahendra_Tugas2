41f9e82f1fe35b931504f1f9c5a698427e365173
Deadila Defiatri <deadiladefiatri04@gmail>
2021-03-12 08:43:40 +0700

    Pengerjaan Tugas

--- I0320023_Soal1_Tugas2.py
+++ I0320023_Soal1_Tugas2.py
@@ -1,5 +1,6 @@
 
 print ("-----Menentukan Luas Persegi Panjang-----")
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("")
 panjang = float(input("Masukkan panjang : "))
 lebar = float(input("Masukkan Lebar : "))
@@ -8,6 +9,7 @@ print ("luas persegi panjang adalah", luas, "satuan")
 print ("")
 
 print ("-----Menghitung Luas Lingkaran------")
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("")
 jari_jari = float(input("Masukkan jari _jari : " ))
 phi = 3.14
@@ -16,18 +18,22 @@ print ("luas lingkaran", luaslingkaran , "satuan" )
 print("")
 
 print ("-----Menghitung Luas Permukaan Kubus-----")
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("")
 sisi = float(input("Masukkan panjang sisi = "))
 LuasPermukaanKubus = 6*sisi*sisi
 print ("Luas Permukaan Kubus adalah", LuasPermukaanKubus, "satuan")
+
 print("")
 print ("-----KONVERSI SUHU CELCIUS KE FAHREINHEIT-----")
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("")
 C = float(input ("Masukkan suhu dalam ºC = "))
 F = ((9/5)*C) + 32
 print ("Suhu dalam ºF =", F )
 print("")
 print ("-----KONVERSI SUHU REAMUR KE FAHREINHEIT-----")
+print ("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("")
 R = float(input ("Masukkan suhu dalam ºR = "))
 K = ((5/4)*R) + 273
